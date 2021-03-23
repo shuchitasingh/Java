e7f97572f5d760ef96f122845df7f582aec1f87d
shuchitasingh <singhshuchita5@gmail.com>
2021-03-23 17:51:10 +0530

    Added:Display One to Thousands digit

---
+++ PrintNumbers.java
@@ -0,0 +1,13 @@
+public class PrintNumbers {
+   public static void printNum(int num) {
+
+      if (num <=1000){
+         System.out.print(num+" ");
+         printNumsssss(num+1);
+      }
+    }
+ public static void main(String[]args){
+   int n=1;
+   printNum(n); 
+    }
+}
